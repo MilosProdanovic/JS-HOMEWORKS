108ac4fe47daf3b231dfb672adb698d608425a5f
MilosProdanovic <mil.prodanovic@gmail.com>
2019-11-23 11:45:33 +0100

    add new files

---
+++ main.js
@@ -0,0 +1,16 @@
+/* TASK1 */
+var Peter = "3500$";
+var Jim = "5000$" ;
+
+console.log (Peter > Jim);
+
+/* TASK2 */
+var John = 38;
+
+console.log ((John + 8) / 2 - 8 + " years old is Mike now.");
+
+/* TASK3 */
+var firstFlock = 78;
+var secondFlock = 57;
+
+console.log (firstFlock - 12 >= secondFlock + 12 - 4);
