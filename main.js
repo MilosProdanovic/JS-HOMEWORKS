082c05cd10cec0691b2860ee3083c2addd26c97e
MilosProdanovic <mil.prodanovic@gmail.com>
2019-12-14 16:43:35 +0100

    add new files

--- main.js
+++ main.js
@@ -1,58 +1,141 @@
 //TASK1
 
-function firstFun(someArr){
-	 secondFun(someArr);
-	 var lowest = someArr[0];
-	 for (var i = 0; i < someArr.length; i++){
-		if (someArr[i] < lowest) {
-			lowest = someArr[i]
-	 	}
-	}
-	 return lowest;
+var person = {
+	name: "Mike",
+	age: 28,
+	married: true
 }
-	
-function secondFun(someArr){
-	var highest = 0;
-	for (var i = 0; i < someArr.length; i++){
-		if (someArr[i] > highest){
-			highest = someArr[i]
-		}
-	}
-	return highest;
+function print(){
+	person.age = 34;
+	delete person.married;
+	console.log(person);
 }
-
-function thirdFun(someArr){
-	var first = firstFun(someArr);
-	var second = secondFun(someArr);
-	console.log (first * second);
-	}
-
-thirdFun([10, 11, 21, 3, 1]);
+print();
 
 //TASK2
 
-function funOne(some){
-	var sameNum = [];
-	for (var i = 0; i < some.length; i++){
-		for(var j = i + 1; j < some.length; j++){
-			if (some[i] === some[j]){
-			sameNum[i] = some[i]}
-		}
-	}return sameNum;
-}
-function arr(some){
-	var delBiggest = funOne(some);
-	var biggest = [0];
-	for(var i = 0; i < delBiggest.length; i++){
-		if(delBiggest[i] > biggest){
-		biggest = delBiggest[i];
-		}
-	}
-	for(var i = 0; i < delBiggest.length; i++){
-		if(delBiggest[i] === biggest){
-			delete delBiggest[i];
-		}
-	}console.log(delBiggest);
+var personOne = {
+	name: "Jack",
+	age: 32,
+	married: true
 }
 
-arr([15, 35, 46, 23, 15, 17, 23, 24, 35, 12, 72, 64, 35, 22, 64]);
+function childrenData(){
+	var children = [{name : "Zoran", age : 6, gender : "male"}, 
+					{name : "Zorana", age : 4, gender: "female"}];	
+	for(var i = 0; i < Object.length; i++){
+		if(Object.keys != this.children){
+			personOne.children = children;	
+		}
+	}console.log(personOne);
+}
+childrenData();
+
+//TASK3
+	
+var students = [ 
+   {
+       name: 'Mike',
+       age: 28,
+       passed: false
+   },
+   {
+       name: 'Anna',
+       age: 23,
+       passed: true
+   },
+   {
+       name: 'Jack',
+       age: 32,
+       passed: true
+   },
+];
+
+function exam(){
+	for (var i = 0; i < students.length; i++){
+		if (students[i].passed  == true ){
+			console.log(students[i].name + " passed exam");
+		}
+	}
+}
+
+exam();
+
+//TASK4
+
+var students = [ 
+   {
+       name: 'Mike',
+       age: 28,
+       passed: false
+   },
+   {
+       name: 'Anna',
+       age: 23,
+       passed: true
+   },
+   {
+       name: 'Jack',
+       age: 32,
+       passed: true
+   },
+];
+
+function repack(){
+	var names = [];
+	var ages = [];
+	var passed = [];
+	for (var i = 0; i < students.length; i++){
+		names[i] = students[i].name;
+		ages[i] = students[i].age;
+		passed[i] = students[i].passed;
+	}console.log(names, ages, passed);
+}
+
+repack();
+
+//TASK5
+
+var person = {
+	name: "Jack",
+	age: 32,
+	married: true
+}
+
+var personNew = new Object();
+personNew.name = person.name;
+personNew.age = person.age;
+personNew.married = person.married;
+
+console.log(personNew.age);
+
+//TASK6
+
+var person = {
+	name: "Mike",
+	age: 28,
+	married: true,
+	nameAge: function (){
+		console.log(this.name + " " + this.age)
+	}
+}
+
+console.log(person.nameAge());
+
+//TASK7
+
+var person = {
+	objectConstructor: function(name, age, married){
+		this.name = name;
+		this.age = age;
+		this.married = married;
+		return person;
+	}
+};
+
+console.log(person.objectConstructor("Djordje", 58, true));	
+
+
+
+
+
