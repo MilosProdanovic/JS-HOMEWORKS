b4c471b3740893391a1e7d492f58ae976c5c0b72
MilosProdanovic <mil.prodanovic@gmail.com>
2020-02-12 10:49:33 +0100

    add new files

--- main.js
+++ main.js
@@ -1,127 +1,101 @@
-
-var arrPic = [{
-    img:"picOne.jpg",
-    name: "Spring"
-    },
-    {
-    img: "picTwo.jpg",
-    name: "Summer"
-    },
-    {
-    img: "picThree.jpg",
-    name: "Autumn"
-    },
-    {
-    img: "picFour.jpg",
-    name: "Winter"
+var moviesArr = [{
+	img: "fight-club.jpg",
+	name: "Fight club"
+},
+{
+	img: "forrest-gump.jpg",
+	name: "Forrest Gump"
+},
+{
+	img: "goodfellas.jpg",
+	name: "Goodfellas"
+},
+{
+	img: "inception.jpg",
+	name: "Inception"
+},
+{
+	img: "one-flew-over-the-cockoos-nest.jpg",
+	name: "One flew over the cockoo's nest"
+},
+{
+	img: "pulp-fiction.jpg",
+	name: "Pulp fiction"
+},
+{
+	img: "schindlers-list.jpg",
+	name: "Schindler's list"
+},
+{
+	img: "the-dark-knight.jpg",
+	name: "The dirk knight"
+},
+{
+	img: "the-godfather.jpg",
+	name: "The godfather"
+},
+{
+	img: "the-god-the-bad-and-the-ugly.jpg",
+	name: "The god, the bad and the ugly"
+},{
+	img: "the-shawshank-redemption.jpg",
+	name: "The Shawshank redemption"
+},
+{
+	img: "twelve-angry-men.jpg",
+	name: "Twelve angry men"
 }];
 
-function addPic(){
-    arrPic.forEach(function(pic){
-        var pictures = document.querySelector('.container');
-        pictures.appendChild(createPic(pic));
-    });
+function addMovies(){
+	moviesArr.forEach(function(art){
+		var movies = document.querySelector('.movies');
+		movies.appendChild(createMovies(art));
+	});
 };
 
+function createMovies(moviesData){
+	var art = document.createElement('div');
+	art.classList.add('moviesData')
 
-function createPic(imgData){
-    var pic = document.createElement('div');
-    pic.classList.add('picData');
-
-    var img = '<img src ="img/' + imgData.img + '" alt= ""/>';
-    var name = '<div>' + imgData.name + '</div>';
-    
-    pic.innerHTML = img + name;
-    return pic;
+	var img = '<img src ="img/' + moviesData.img + '" alt= ""/>';
+	var name = '<div>' + moviesData.name + '</div>';
+	var remove = '<button><img src ="img/remove.png"></button>';
+	art.innerHTML = remove + img + name;
+	return art;
 };
 
-addPic();
+addMovies();
 
-function addThumb(){
-    arrPic.forEach(function(thumb){
-        var tn = document.querySelector('.nails');
-        tn.appendChild(createThumb(thumb));
+var searchBar = document.getElementById("input");
+var btn = document.getElementById("button")
+var home = document.getElementById("reload")
+home.style.display = "none";
 
-    });
-};
+btn.addEventListener('click', function(e){
+    var term = searchBar.value.toLowerCase();
+    var movies = document.getElementsByClassName('moviesData');
+    var title  = document.querySelectorAll('div.moviesData > div');
+    	
+    	for(var i = 0; i < movies.length; i++){
+   			if(term === title[i].innerText.toLowerCase()){
+   			movies[i].style.display = "block";
+			}else{movies[i].style.display = "none"}
+		}
 
-addThumb();
+	home.style.display = "block";
+	searchBar.value = "";
+});
 
-function createThumb(thumbData){
-    var thumb = document.createElement('span');
-    thumb.classList.add('thumbnails');
-
-    var img = '<img src ="img/' + thumbData.img + '" alt= ""/>';
-    thumb.innerHTML = img;
-    return thumb;
+function remBtn(){
+	var remove = document.querySelectorAll('div.moviesData > button');
+	var i = 0;
+	for(var i = 0; i < remove.length; i++){
+		remove[i].addEventListener('click', function(e){
+			var mov = document.getElementsByClassName('moviesData');
+			e.path[2].style.display = "none";
+			home.style.display = "block";
+		})
+	}
 }
 
-var slideNum = 0; 
-
-// SWITCH SLIDER//
-   
-// function slider(){ 
-//     var images = document.getElementsByClassName("picData"); 
-//     var dots = document.getElementsByClassName("thumbnails");   
-
-//     for (var i = 0; i < images.length; i++){ 
-//         images[i].style.display = "none";  
-//     };
-
-//     slideNum++;  
-   
-//     if (slideNum > images.length){ 
-//         slideNum = 1; 
-//     };
-
-//     for (i = 0; i < dots.length; i++) { 
-//         dots[i].className = dots[i].className.replace(" active", ""); 
-//     }; 
-   
-//     dots[slideNum - 1].className += " active"; 
-//     images[slideNum - 1].style.display = "flex"; 
-
-//     setTimeout(slider, 5000);  
-// };
-
-// slider();
-
-//SLIDE SLIDER//
-
-var slideNumber = 0;
-var dots = document.getElementsByClassName("thumbnails");
-dots[slideNum].className += " active";
-
-function slide(){
-    var slideContainer = document.querySelector('div');
-    var images = document.getElementsByClassName("picData");
-    var thumb = document.getElementsByClassName("nails");
-    
-    slideContainer.style.marginLeft = slideNumber + "px"; 
-    slideNumber = slideNumber - 1000;
-    
-    dots[slideNum].className = dots[slideNum].className.replace(" active", "");
-
-};
-
- setInterval(function()
-        {slide(); app()}, 5000);
-
- function app(){
-    var container = document.querySelector('div');
-    var first = document.querySelector('.picData');
-   
-    container.appendChild(first);
-    container.style.marginLeft = "0px";
-    
-    slideNum = slideNum + 1;
-    
-    if(slideNum > dots.length -1){
-        slideNum = 0; 
-    };
-
-    dots[slideNum].className += " active"; 
- };
-
-
-
+remBtn();
